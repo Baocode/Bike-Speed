dd5018c50f13ce938054089ffecca27471594f38
Arnaud Lacroix <arnaud.lacroix@emundus.io>
2019-08-30 11:18:56 +0200

    Initial Commit

---
+++ Bike Speed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bike Speed
+//
+//  Created by Arnaud Lacroix on 30/08/2019.
+//  Copyright © 2019 BaoCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
