fa951cbce3fa42c95cd2314ceee288a8d3de4725
Arnaud Lacroix <arnaud.lac@gmail.com>
2019-11-28 16:03:29 +0100

    First Commit

--- Bike Speed/AppDelegate.swift
+++ Bike Speed/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import CoreLocation
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let locationManager = LocationManager.shared
+        locationManager.requestWhenInUseAuthorization()
         return true
     }
 
---
+++ Bike Speed/LocationManager.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManager.swift
+//  Bike Speed
+//
+//  Created by Arnaud Lacroix on 06/09/2019.
+//  Copyright © 2019 BaoCode. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager {
+  static let shared = CLLocationManager()
+  
+    
+  private init() { }
+}
