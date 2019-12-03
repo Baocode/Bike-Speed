cf2359e93f643797f3ef7227500ad76e10ce38a2
Arnaud Lacroix <arnaud.lac@gmail.com>
2019-12-03 14:08:24 +0100

    mise en place d'un bouton pause ajout d'un filtrage de la précision avant le calcul de la distance

--- Bike Speed/ViewController.swift
+++ Bike Speed/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var actualDistance: CLLocationDistance = CLLocationDistance()
     var distanceMgr: DistanceMgr = DistanceMgr()
     var previousLocation: CLLocation = CLLocation()
+    var pauseOn:Bool = false
     
     
     
     @IBOutlet weak var speedLabelUI: UILabel!
     @IBOutlet weak var altitudeLabelUI: UILabel!
     @IBOutlet weak var distanceLabelUi: UILabel!
+    @IBOutlet weak var pauseButtonUI: RoundButton!
     
     
     @IBOutlet weak var map: MKMapView!
@@ -45,32 +47,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
         }
-        
         if let location = locationManager.location{
-            if (location.speed <= 0)
-            {
-                speed = 0
-            }else{
-                speed = location.speed*3600/1000;
-            }
-            actualDistance = distanceMgr.calculDistanceTotale(actualPosition: location)
-            altitude = location.altitude
-            map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+            updateInformation(location: location)
         }
-        speedLabelUI.text = String(format: "%.2f", speed)
-        altitudeLabelUI.text = String(format: "%.2f", altitude)
-        distanceLabelUi.text = String(format: "%.2f", actualDistance)
         
 
     }
+    
+    @IBAction func pauseButton(_ sender: Any) {
+
+        if pauseOn {
+            locationManager.startUpdatingLocation()
+            pauseButtonUI.backgroundColor = UIColor.white
+            pauseOn = false
+        }else{
+            pauseOn = true
+            locationManager.stopUpdatingLocation()
+            pauseButtonUI.backgroundColor = UIColor.red
+        }
+        
+    }
+    
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("hello")
-        let location: CLLocation = manager.location!;
+        updateInformation(location: manager.location!);
+    }
+    
+    func updateInformation(location: CLLocation){
+        if !pauseOn {
         let eventDate: Date = location.timestamp;
         let howRecent: TimeInterval = eventDate.timeIntervalSinceNow;
         if (abs(howRecent) < 15.0) {
@@ -82,38 +90,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 speed = location.speed*3600/1000;
             }
                 altitude = location.altitude
+            if (location.horizontalAccuracy < 5) {
+                actualDistance = distanceMgr.calculDistanceTotale(actualPosition: location)
             }
-        actualDistance = distanceMgr.calculDistanceTotale(actualPosition: location)
-        speedLabelUI.text = String(format: "%.2f", speed)
-        altitudeLabelUI.text = String(format: "%.2f", altitude)
-        distanceLabelUi.text = String(format: "%.2f", actualDistance)
-        map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+            }
+           
+            speedLabelUI.text = String(format: "%.2f", speed)
+            altitudeLabelUI.text = String(format: "%.2f", altitude)
+            distanceLabelUi.text = String(format: "%.2f", actualDistance)
+            map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+        }
     }
     
-//    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-//
-//        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-//              print("locations = \(locValue.latitude) \(locValue.longitude)")
-//
-////        print("hello")
-////        if let lastLocations = locations {
-//            let location: CLLocation = lastLocations[lastLocations.endIndex] as! CLLocation;
-//            let eventDate: Date = location.timestamp;
-//            let howRecent: TimeInterval = eventDate.timeIntervalSinceNow;
-//            if (abs(howRecent) < 15.0) {
-//                // If the event is recent, do something with it.
-//                NSLog("latitude %+.6f, longitude %+.6f\n",location.coordinate.latitude, location.coordinate.longitude);
-//                if (location.speed <= 0){
-//                    speed = 0
-//                }else{
-//                    speed = location.speed;
-//                }
-//                altitude = location.altitude
-//            }
-//            print("speed = \(speed)");
-//            speedLabelUI.text = "\(speed)"
-//            altitudeLabelUI.text = "\(altitude)"
-//        }
     
     
     private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
