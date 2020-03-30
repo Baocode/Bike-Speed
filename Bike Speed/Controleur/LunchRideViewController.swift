03d13ea0f11260192ffb74a1e6e062015c1032bb
Arnaud Lacroix <arnaud.lac@gmail.com>
2020-03-30 17:31:02 +0200

    Amélioration UI, mise en place de la navigation Start Stop
    tab bar view pour l'historique

---
+++ Bike Speed/Controleur/LunchRideViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LunchRideViewController.swift
+//  Bike Speed
+//
+//  Created by Arnaud Lacroix on 30/03/2020.
+//  Copyright © 2020 BaoCode. All rights reserved.
+//
+
+import UIKit
+
+class LunchRideViewController: UIViewController {
+
+    @IBOutlet weak var lunchRideButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func lunchRideAction(_ sender: Any) {
+        
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
